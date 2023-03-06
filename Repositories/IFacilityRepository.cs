b3d0dfc816a938d5c86922386990388bf63b6a25
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2023-03-06 11:37:21 +0200

    Add Facility repository

---
+++ Repositories/IFacilityRepository.cs
@@ -0,0 +1,13 @@
+﻿using CampingMap.API.Models;
+
+namespace CampingMap.API.Repositories
+{
+    public interface IFacilityRepository
+    {
+        Task<IEnumerable<Facility>> GetFacilities();
+
+        Task<Facility> AddFacility(Facility facility);
+
+        Task<Facility> DeleteFacility(Guid id);
+    }
+}
