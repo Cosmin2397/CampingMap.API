408417bb08c81addd5f2bc1891746ae6a707dd45
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2023-03-06 11:45:57 +0200

    Add Facility Controller

---
+++ Controllers/FacilitiesController.cs
@@ -0,0 +1,68 @@
+﻿using CampingMap.API.Models;
+using CampingMap.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampingMap.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacilitiesController : ControllerBase
+    {
+        private readonly IFacilityRepository _facilityRepository;
+
+        public FacilitiesController(IFacilityRepository facilityRepository)
+        {
+                _facilityRepository = facilityRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Facility>>> GetFacilities()
+        {
+            var facilities = await _facilityRepository.GetFacilities();
+            return Ok(facilities);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Facility>> PostFacility(Facility facility)
+        {
+            try
+            {
+
+                await _facilityRepository.AddFacility(facility);
+                if (facility == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(facility);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFacility(Guid id)
+        {
+            try
+            {
+
+                var facility = await _facilityRepository.DeleteFacility(id);
+                if (facility == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
