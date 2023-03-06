23f7805bc3c3c292037cab8323007b2346c31dee
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2023-03-06 11:03:39 +0200

    Add ILocationRepository

--- Repositories/ILocationRepository.cs
+++ Repositories/ILocationRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace CampingMap.API.Repositories
+﻿using CampingMap.API.Models;
+
+namespace CampingMap.API.Repositories
 {
     public interface ILocationRepository
     {
+        Task<IEnumerable<Location>> GetLocations();
+
+        Task<IEnumerable<Location>> GetRegionCampings(string region);
+
+        Task<IEnumerable<Location>> GetCountyampings(string county);
+
+        Task<IEnumerable<Location>> GetCityCampings(string city);
+
+        Task<Location> GetLocationById(Guid id);
+
+        Task<Location> AddLocation(Location location);
+
+        Task<Location> UpdateLocation(Guid id, Location location);
+
+        Task<Location> DeleteLocation(Guid id);
     }
 }
