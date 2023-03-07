8ba0f5d03777b885339935ef65defca52d76cdca
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2023-03-07 18:16:03 +0200

    Add ICampingFacility

---
+++ Repositories/ICampingFacilitiesRepository.cs
@@ -0,0 +1,19 @@
+﻿using CampingMap.API.Models;
+
+namespace CampingMap.API.Repositories
+{
+    public interface ICampingFacilitiesRepository
+    {
+        Task<IEnumerable<CampingFacilities>> GetCampingFacilities();
+
+        Task<IEnumerable<CampingFacilities>> GetCampingFacilities(Guid campingId);
+
+        Task<CampingFacilities> GetCampingFacilitiesById(Guid id);
+
+        Task<CampingFacilities> AddCampingFacilities(CampingFacilities location);
+
+        Task<CampingFacilities> UpdateCampingFacility(Guid id, CampingFacilities location);
+
+        Task<CampingFacilities> DeleteCampingFacility(Guid id);
+    }
+}
